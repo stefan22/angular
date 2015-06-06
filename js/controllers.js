3751fd8fde3217e16d51f8748ebc29ceef812a08
Stefan Lissia <stefan.emailacct@gmail.com>
2015-06-06 16:36:01 +0100

    protected angular.min from minification

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,10 @@
 var myApp = angular.module('myApp',[]);
 
-myApp.controller('MyController', function MyController($scope, $http) {
+myApp.controller('MyController',['$scope', '$http', function($scope, $http) {
 
     $http.get('js/data.json').success(function(data) {
         $scope.artists = data;
     });
 
 
-});
+}]);
