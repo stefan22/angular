0282accc69c9b5ce7d841f62abcd1cf88b323d7f
Stefan Lissia <stefan.emailacct@gmail.com>
2015-06-06 16:07:12 +0100

    parsed json file from a file using $http

--- js/controllers.js
+++ js/controllers.js
@@ -1,81 +1,10 @@
 var myApp = angular.module('myApp',[]);
 
-myApp.controller('MyController', function MyController($scope) {
+myApp.controller('MyController', function MyController($scope, $http) {
 
-  $scope.artists = [
-    {
-      'name': "Mario Cockinghead",
-      'shortname': "M_Head",
-      'reknown': "Royal Academy of Painting",
-      'bio':"Cockinghead has just finished his book blaha blaha blah"
-    },
-
-    {
-      'name': "Thomas Cockinghat",
-      'shortname': "M_Hat",
-      'reknown': "Royal Academy of Plumbing",
-      'bio':"Cockinghat has just finished his book blaha blaha blah"
-    },
-
-    {
-      'name': "Rob Cockingfuss",
-      'shortname': "M_Fuss",
-      'reknown': "Royal Academy of Air Conditioning",
-      'bio':"Cockingfuss has just finished his book blaha blaha blah"
-    },
-
-    {
-      'name': "Larry Cockingshoes",
-      'shortname': "M_Shoes",
-      'reknown': "Royal Academy of Lumber",
-      'bio':"Cockingshoes has just finished his book blaha blaha blah"
-    },
-
-    {
-      'name': "Gus Cockingmass",
-      'shortname': "M_Mass",
-      'reknown': "Royal Academy of Aeronautics",
-      'bio':"Cockingmass has just finished his book blaha blaha blah"
-    },
-
-    {
-      'name': "Tim Cockingblown",
-      'shortname': "M_Blown",
-      'reknown': "Royal Academy of Fishing",
-      'bio':"Cockingblown has just finished his book blaha blaha blah"
-    },
-
-    {
-      'name': "Manny Cockingjunk",
-      'shortname': "M_Junk",
-      'reknown': "Royal Academy of Agriculture",
-      'bio':"Cockingjunk has just finished his book blaha blaha blah"
-    },
-
-    {
-      'name': "Leo Cockingjoke",
-      'shortname': "M_Joke",
-      'reknown': "Royal Academy of Computing",
-      'bio':"Cockingjoke has just finished his book blaha blaha blah"
-    },
-
-    {
-      'name': "Jeff Cockingnothing",
-      'shortname': "M_Nothing",
-      'reknown': "Royal Academy of Pesticides",
-      'bio':"Cockingnothing has just finished his book blaha blaha blah"
-    },
-
-    {
-      'name': "Sal Cockingstar",
-      'shortname': "M_Star",
-      'reknown': "Royal Academy of Blogging",
-      'bio':"Cockingstar has just finished his book blaha blaha blah"
-    },
-
-
-
-  ]
+    $http.get('js/data.json').success(function(data) {
+        $scope.artists = data;
+    });
 
 
 });
